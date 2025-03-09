44c8bfabefe2ccab894d7f02aaa4ebb57466e22c
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2025-03-09 18:14:44 +0100

    added env variables

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use axum::{routing::get, Router};
 use controller::user_controller;
 use repository::user_repository::PostgresUserRepository;
@@ -11,7 +13,18 @@ mod service;
 
 #[tokio::main]
 async fn main() {
-    let pool = PgPool::connect("postgres://kartvisare:kartvisare@localhost:5432/kartvisare")
+    let db_host = env::var("DB_HOST").expect("DB_HOST must be set in the environment");
+    let db_port = env::var("DB_PORT").expect("DB_PORT must be set in the environment");
+    let db_user = env::var("DB_USER").expect("DB_USER must be set in the environment");
+    let db_password = env::var("DB_PASSWORD").expect("DB_PASSWORD must be set in the environment");
+    let db_name = env::var("DB_NAME").expect("DB_NAME must be set in the environment");
+
+    let database_url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        db_user, db_password, db_host, db_port, db_name
+    );
+
+    let pool = PgPool::connect(&database_url)
         .await
         .expect("Failed to connect to database");
 
