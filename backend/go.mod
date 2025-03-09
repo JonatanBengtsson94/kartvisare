62f601e48ea3201284f41aeb03df81fe4c130865
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2025-03-09 16:38:39 +0100

    removed go backend

--- backend/go.mod
+++
@@ -1,5 +0,0 @@
-module backend
-
-go 1.24.0
-
-require github.com/lib/pq v1.10.9 // indirect
--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DB_HOST=localhost
-export DB_PORT=5432
-export DB_USER=kartvisare
-export DB_PASSWORD=kartvisare
-export DB_NAME=kartvisare
-
-go run cmd/main.go
