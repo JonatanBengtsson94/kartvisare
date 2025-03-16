2078f5329c2eb9f8d839e45ece4df1b86b8b9fda
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2025-03-16 11:25:39 +0100

    created map inside canvas

--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -1,9 +1,65 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { Wms } from '../types/wmsTypes.ts'
+import { Map as OlMap, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
 
-const Canvas: React.FC = () => {
+interface CanvasProps{
+  wms: Wms[];
+}
+
+const Canvas: React.FC<CanvasProps> = ({ wms }) => {
   const mapRef = useRef<HTMLDivElement>(null);
+  const mapInstance = useRef<OlMap | null>(null);
+  const layerInstances = useRef<Map<number, TileLayer>>(new Map());
+
+  useEffect(() => {
+    const currentLayerInstances = layerInstances.current;
+
+    if (!mapInstance.current && mapRef.current) {
+      mapInstance.current = new OlMap({
+        target: mapRef.current,
+        layers: [],
+        view: new View({
+        }),
+      });
+    }
+
+    const currentMap = mapInstance.current;
+    if (currentMap) {
+      wms.forEach((wms) => {
+        if (!layerInstances.current.has(wms.id)) {
+          const tileLayer = new TileLayer({
+            source: new TileLayer({
+              url: wms.url,
+              params: {
+                LAYERS: wms.layers,
+              },
+            }),
+        });
+
+        currentMap.addLayer(tileLayer);
+        currentLayerInstances.set(wms.id, tileLayer);
+      }
+    });
+
+    layerInstances.current.forEach((layer, id) => {
+      if (!wms.some((wms) => wms.id === id)) {
+        currentMap.removeLayer(layer);
+        currentLayerInstances.delete(id);
+      }
+    });
+  }
+
+  return () => {
+    const currentMap = mapInstance.current;
+    if (currentMap) {
+      currentMap.getLayers().clear();
+      currentLayerInstances.clear();
+    }
+  };
+}, [wms]);
 
   return <div className="canvas" ref={mapRef} />
-}
+};
 
 export default Canvas;
--- frontend/src/types/wmsTypes.ts
+++ frontend/src/types/wmsTypes.ts
@@ -1,6 +1,14 @@
 export interface Wms {
   id: number,
   name: string,
+  description: string,
+  layers: string[],
+  url: string,
+  version: string,
+  isActive: boolean,
+  authType: string,
+  authPassword: string,
+  authUsername: string,
 }
 
 export interface WmsGroup {
