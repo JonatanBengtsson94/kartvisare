993cccd72a8018ebc8e47893d861df6f0c6b7472
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2025-03-18 18:03:31 +0100

    setup database for role based access

--- databas/localhost/testdata.sql
+++ databas/localhost/testdata.sql
@@ -2,6 +2,20 @@ INSERT INTO wms_groups (name) VALUES ('World');
 INSERT INTO wms_groups (name, parent_id) VALUES ('Usa', 1);
 INSERT INTO wms_groups (name, parent_id) VALUES ('Europe', 1);
 
+INSERT INTO users (username) VALUES ('John');
+INSERT INTO users (username) VALUES ('Jane');
+
+INSERT INTO user_groups (group_name) VALUES ('Standard');
+INSERT INTO user_groups (group_name) VALUES ('Admins');
+
+INSERT INTO user_group_membership (user_id, group_id) VALUES (1, 1);
+INSERT INTO user_group_membership (user_id, group_id) VALUES (2, 2);
+
 INSERT INTO wms (name, layers, url, is_active, group_id) 
 VALUES ('States', ARRAY['topp:states'], 'http://localhost:8001/geoserver/wms', true, 2);
 
+INSERT INTO wms (name, layers, url, is_active, group_id)
+VALUES ('Lakes', ARRAY['topp:tasmania_water_bodies'], 'http://localhost:8001/geoserver/wms', true, 3);
+
+INSERT INTO wms_user_groups_membership (wms_id, group_id) VALUES (1, 1);
+INSERT INTO wms_user_groups_membership (wms_id, group_id) VALUES (2, 2);
