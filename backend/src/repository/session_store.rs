431ac1756533e461ec492ea07135ac0b6f828744
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2025-03-29 16:55:43 +0100

    feat: created function to get session from redis

--- backend/src/repository/session_store.rs
+++ backend/src/repository/session_store.rs
@@ -3,9 +3,9 @@ use crate::domain::session::Session;
 use super::repo_error::RepoError;
 use async_trait::async_trait;
 use redis::{AsyncCommands, Client};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct SessionData {
     user_id: i32,
     is_admin: bool,
@@ -24,16 +24,34 @@ impl RedisSessionStore {
 
 #[async_trait]
 pub trait SessionStore {
-    async fn load_session(&self, session_id: &str) -> Option<Session>;
+    async fn load_session(&self, session_id: &str) -> Result<Session, RepoError>;
     async fn save_session(&self, user_id: i32, is_admin: bool) -> Result<Session, RepoError>;
 }
 
 #[async_trait]
 impl SessionStore for RedisSessionStore {
-    async fn load_session(&self, session_id: &str) -> Option<Session> {
-        // TODO
-        None
+    async fn load_session(&self, session_id: &str) -> Result<Session, RepoError> {
+        let mut con = self
+            .client
+            .get_multiplexed_async_connection()
+            .await
+            .map_err(RepoError::RedisError)?;
+
+        let session_data: Option<String> =
+            con.get(session_id).await.map_err(RepoError::RedisError)?;
+        match session_data {
+            Some(data) => match serde_json::from_str::<SessionData>(&data) {
+                Ok(session_data) => Ok(Session {
+                    session_id: session_id.to_string(),
+                    user_id: session_data.user_id,
+                    is_admin: session_data.is_admin,
+                }),
+                Err(e) => Err(RepoError::SerializationError(e)),
+            },
+            None => Err(RepoError::NotFound),
+        }
     }
+
     async fn save_session(&self, user_id: i32, is_admin: bool) -> Result<Session, RepoError> {
         let session_data = SessionData { user_id, is_admin };
         let session_json =
